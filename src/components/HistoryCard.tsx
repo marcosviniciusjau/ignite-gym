5e783cc2fb03b1f5ac8aae9d6938e1fb9dffd3d9
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-06-26 13:54:08 -0300

    creating the history card

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,33 @@
+import { HStack, VStack, Heading, Text } from "native-base";
+
+export function HistoryCard(){
+  return (
+    <HStack
+      w="full"
+      px={5}
+      py={4}
+      mb={3}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <VStack mr={5} flex={1}>
+        <Heading
+          color="white"
+          fontSize="md"
+          fontFamily="heading"
+          textTransform="capitalize"
+          numberOfLines={1}
+          >
+          Costas
+        </Heading>
+        <Text color="gray.100" fontSize="lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+        <Text color="gray.300" fontSize="md">08:56</Text>
+      </VStack>
+    </HStack>
+          
+  )
+}
